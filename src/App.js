16235771e354c266d67a1ddaa6a6f244713147e6
Arjun Umathanu <AUmathanu@athene.com>
2021-10-04 18:39:28 +0530

    Updated child components list in App Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Books } from "./components/Books";
+import { Authors } from "./components/Authors";
+import CardUI from "./components/CardUI";
+import Layout from "./components/Layout";
 
 function App() {
 	return (
 		<div className="App">
+			<Layout />
 			{/* <header className="App-header"></header> */}
 			{/* <img src={logo} className="App-logo" alt="logo" /> */}
-			<p>E-Bookstore</p>
+			{/* <p>E-Bookstore</p>
+			<CardUI />
 			<Books />
+			<Authors /> */}
 		</div>
 	);
 }
