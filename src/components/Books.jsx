49e0845027f40cc1df577cca346f57a40f895218
Arjun Umathanu <AUmathanu@athene.com>
2021-10-04 18:37:32 +0530

    Replaced window.fetch with axios and added some styles

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,44 +1,62 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+
+import CardUI from "./CardUI";
 
 // export const baseURL = process.env.REACT_APP__BOOK_SERVICE_BASE_URI;
 // const baseURL = "https://jsonplaceholder.typicode.com/posts";
 // const baseURL = "http://localhost:8000/books";
 const baseURL = process.env.REACT_APP_BOOK_SERVICE_BASE_URI + "/books";
 console.log(baseURL);
+
 export const Books = () => {
-	const [books, setBooks] = React.useState([]);
-	const [error, setError] = React.useState(null);
+	const [status, setStatus] = useState("idle");
+	const [books, setBooks] = useState([]);
+	const [error, setError] = useState(null);
+
+	const isLoading = status === "loading";
+	const isSuccess = status === "success";
 
 	const fetchBooks = async (url) => {
 		try {
-			const response = await window.fetch(url);
-			const respJson = await response.json();
-			console.log(respJson);
-			setBooks(respJson);
+			const response = await axios(url);
+			console.log(response);
+			setBooks(response?.data);
+			setStatus("success");
 		} catch (err) {
 			console.log("err", err);
 			setError(err);
 		}
 	};
 
-	if (error) throw error;
+	// if (error) throw error;
 
-	React.useEffect(() => {
+	useEffect(() => {
 		fetchBooks(baseURL);
 	}, []);
 
 	return (
 		<div>
 			<h3>Books</h3>
-			<ul>
-				{books.map((book) => (
-					<div key={book.isbn}>
-						<li>Title: {book.title}</li>
-						<li>ISBN: {book.isbn}</li>
-						<li>Author: written by {book.author.name}</li>
-					</div>
-				))}
-			</ul>
+			<Box sx={{ flexGrow: 1 }}>
+				<Grid container spacing={2}>
+					{isSuccess ? (
+						books?.length ? (
+							books.map((book) => {
+								return (
+									<Grid item xs={4}>
+										<CardUI book={book} {...book} />
+									</Grid>
+								);
+							})
+						) : (
+							<h3>No books found</h3>
+						)
+					) : null}
+				</Grid>
+			</Box>
 		</div>
 	);
 };
