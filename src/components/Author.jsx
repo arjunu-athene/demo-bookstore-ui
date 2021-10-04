937bc1bb107e84157023d4a726c28d32c022537b
Arjun Umathanu <AUmathanu@athene.com>
2021-10-05 04:41:46 +0530

    Added individual author detail page with respective authored books list

---
+++ src/components/Author.jsx
@@ -0,0 +1,65 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import UsersList from "./UsersList";
+import Stack from "@mui/material/Stack";
+import { useParams } from "react-router-dom";
+
+// export const baseURL = process.env.REACT_APP__BOOK_SERVICE_BASE_URI;
+// const baseURL = "https://jsonplaceholder.typicode.com/posts";
+// const baseURL = "http://localhost:8000/books";
+const baseURL = process.env.REACT_APP_BOOK_SERVICE_BASE_URI + "/authors";
+console.log(baseURL);
+
+const Author = () => {
+	const [status, setStatus] = useState("idle");
+	const [author, setAuthor] = useState(null);
+	const [error, setError] = useState(null);
+	let { id } = useParams();
+
+	const isLoading = status === "loading";
+	const isSuccess = status === "success";
+
+	const fetchauthors = async (url) => {
+		setStatus("loading");
+		try {
+			const response = await axios(url);
+			console.log("response", response);
+			setAuthor(response?.data);
+			setStatus("success");
+		} catch (err) {
+			console.log("err", err);
+			setError(err);
+		}
+	};
+
+	// if (error) throw error;
+
+	useEffect(() => {
+		fetchauthors(baseURL + `/${id}`);
+	}, [id]);
+
+	console.log("Author Data: ", author);
+
+	return (
+		<>
+			{isLoading ? (
+				<h3>Loading.....</h3>
+			) : (
+				<Stack spacing={2}>
+					{isSuccess ? (
+						author?.id ? (
+							<div key={author?.id}>
+								<UsersList author={author} />
+								{/* <li>Name: {author?.name}</li> */}
+							</div>
+						) : (
+							<h3>No author found</h3>
+						)
+					) : null}
+				</Stack>
+			)}
+		</>
+	);
+};
+
+export default Author;
