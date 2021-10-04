2895fd3ba1894f9aa466b49d9763c65958c5199d
Arjun Umathanu <AUmathanu@athene.com>
2021-10-05 00:41:12 +0530

    Added Routes for books and authors and click on it to navigate to their page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import { Books } from "./components/Books";
 import { Authors } from "./components/Authors";
 import CardUI from "./components/CardUI";
 import Layout from "./components/Layout";
+import PrimarySearchAppBar from "./components/Nav";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
 	return (
 		<div className="App">
-			<Layout />
+			{/* <PrimarySearchAppBar /> */}
+			<Router>
+				<Layout />
+			</Router>
+
 			{/* <header className="App-header"></header> */}
 			{/* <img src={logo} className="App-logo" alt="logo" /> */}
 			{/* <p>E-Bookstore</p>
